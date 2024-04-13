1947ac34c94a9b48c30f91bf78e08099ddefa5d4
Jon Toussaint <jondtoussaint@gmail.com>
2024-04-13 14:02:00 -0400

    add xcode project to shared repo

---
+++ MealSwap/MealSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealSwapApp.swift
+//  MealSwap
+//
+//  Created by Jon Toussaint on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MealSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
